c029a68fa7768613948ca901caf25c8fc07a079d
jamesbinnie <99457931+jamesbinnie@users.noreply.github.com>
2022-04-29 14:33:38 +1000

    Update tracking.js

--- assets/js/tracking.js
+++ assets/js/tracking.js
@@ -1,9 +1,21 @@
+var hero_cta_selector = document.querySelector('#header > div > ul > li > a')
+
 dataLayer = window.dataLayer || [];
-document.querySelector('#header > div > ul > li > a').addEventListener('click', function(){
+
+hero_cta_selector.addEventListener('click', function(){
     dataLayer.push({
     'event': 'gaClick',
     'event_category': 'Button',
     'event_action': 'Click',
+    'event_label': "Hero CTA",
+  });
+});
+
+hero_cta_selector.addEventListener('mouseover', function(){
+    dataLayer.push({
+    'event': 'gaClick',
+    'event_category': 'Button',
+    'event_action': 'Hover',
     'event_label': "Hero CTA",
   });
 });
