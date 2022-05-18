7e448b3ccd46ee7ad9760495bf06d750e089864a
jamesbinnie <99457931+jamesbinnie@users.noreply.github.com>
2022-05-18 12:00:31 +1000

    Update tracking.js

--- assets/js/tracking.js
+++ assets/js/tracking.js
@@ -3,19 +3,27 @@ var hero_cta_selector = document.querySelector('#header > div > ul > li > a')
 dataLayer = window.dataLayer || [];
 
 hero_cta_selector.addEventListener('click', function(){
+    var hero_cta_selector_classes = hero_cta_selector.className;
+    var hero_cta_selector_classes = hero_cta_selector.textContent;
     dataLayer.push({
     'event': 'ga_click',
     'event_category': 'button',
     'event_action': 'click',
     'event_label': "hero_cta",
+    'link_classes': hero_cta_selector_classes,
+    'link_text': hero_cta_selector_text
   });
 });
 
 hero_cta_selector.addEventListener('mouseover', function(){
+    var hero_cta_selector_classes = hero_cta_selector.className;
+    var hero_cta_selector_classes = hero_cta_selector.textContent;
     dataLayer.push({
     'event': 'ga_hover',
     'event_category': 'button',
     'event_action': 'hover',
     'event_label': "hero_cta",
+    'link_classes': hero_cta_selector_classes,
+    'link_text': hero_cta_selector_text
   });
 });
